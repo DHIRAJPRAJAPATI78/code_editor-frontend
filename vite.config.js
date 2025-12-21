6fe301e0fe2257caf59aec15803240dfc2c8410c
DHIRAJPRAJAPATI78 <dhirajkumarprajapati1200@gmail.com>
2025-12-21 18:15:26 +0530

    remove logs

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
+  plugins: [react(),tailwindcss(),],
 })
